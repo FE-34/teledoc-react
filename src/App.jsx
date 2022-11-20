46b96ddb92cdbc4be734c9f20098244afb7f13d8
Rizky Merdika Agusta <rizkymerdika@gmail.com>
2022-11-20 21:16:57 +0700

    Revisi List Dokter

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import {Routes, Route} from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Profilpage from './pages/Profilpage'
 import Chatpage from './pages/Chatpage'
-import Listdokter from './pages/Listdokter'
 
 
 function App() {
@@ -15,8 +15,8 @@ function App() {
         <Route path='/' element={<Homepage/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/profil' element={<Profilpage/>}/>
         <Route path='/chat/:id' element={<Chatpage/>}/>
-        <Route path='/listdokter' element={<Listdokter/>}/>
       </Routes>
     </div>
   )
