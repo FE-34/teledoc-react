fe2f8a021ef2878e61f9cbea8c1e94914bd62ff7
Cheanggi <113913922+Cheanggi@users.noreply.github.com>
2022-11-20 15:23:59 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Listdokter from './Pages/Listdokter';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import { Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Routes>
+            <Route path='/listdokter' element={<Listdokter />}/>
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Listdokter.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../Component/Navbar";
+import Doktercard from "../Component/Doktercard";
+
+
+function Listdokter() {
+  return (
+   <>
+        <Navbar />
+        <Doktercard />
+   </>
+  )
+}
+
+export default Listdokter
