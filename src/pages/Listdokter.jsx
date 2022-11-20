a8cb1328b9c95af8430f6597619e648ea0468d82
dynaudio <dynaudioputra25@gmail.com>
2022-11-20 19:06:27 +0800

    Chat

---
+++ src/pages/Listdokter.jsx
@@ -0,0 +1,14 @@
+import Nav from "../components/Nav";
+import Doktercard from "../components/Doktercard";
+
+
+function Listdokter() {
+  return (
+   <>
+        <Nav />
+        <Doktercard />
+   </>
+  )
+}
+
+export default Listdokter
