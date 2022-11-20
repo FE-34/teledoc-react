5c8c753600c0dc7a238d1f1274b2b9a9731f617f
Cheanggi <113913922+Cheanggi@users.noreply.github.com>
2022-11-20 18:18:31 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import './App.css';
-import Listdokter from './Pages/Listdokter';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import { Routes, Route } from 'react-router-dom';
+import './App.css'
+import Homepage from './pages/Homepage'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import {Routes, Route} from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Chatpage from './pages/Chatpage'
+import Listdokter from './pages/Listdokter'
 
 
 function App() {
   return (
     <div className="App">
-        <Routes>
-            <Route path='/listdokter' element={<Listdokter />}/>
-        </Routes>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/chat/:id' element={<Chatpage/>}/>
+        <Route path='/listdokter' element={<Listdokter/>}/>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
